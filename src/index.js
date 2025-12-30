7f73cbc902f191f4772c849d3dba006225cf8e8e
Zainabfatimahh <xainabfatimahh@gmail.com>
2025-12-30 18:36:05 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// ✅ IMPORT YOUR CSS HERE
+import "./style.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
